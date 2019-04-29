77fd7ef0ea03a3f91a0817cb6742392dcd1ab1bb
Iain Hunter <iain.hunter@outlook.com>
2019-04-29 22:51:39 +0100

    Test deploy

---
+++ devops/deploy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+if [ $IS_PULL_REQUEST == true ]
+    then
+        echo Not Deploying Build $BUILD_NUMBER - Branch is $BRANCH, Is Pull Request is $IS_PULL_REQUEST
+        return
+fi
+
+# Only deploy to Staging if we're on develop
+if [ $BRANCH == "master" ]
+    then
+        # Deploy to S3
+        echo Deploying to S3
+        aws s3 sync build/ s3://www.iain-hunter.com
+fi
